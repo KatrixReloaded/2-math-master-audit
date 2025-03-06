64e0d4eca92af5d408a4144461465f89dbb22b78
KatrixReloaded <kartikeyasureka@gmail.com>
2025-03-06 19:36:17 +0530

    TODO: Fix later

--- test/HalmosTest.t.sol
+++ test/HalmosTest.t.sol
@@ -8,17 +8,19 @@ contract HalmosTest is SymTest, Test {
     /// @dev command required to run this function -> halmos --function check_mulWadUpDoesntRevert --solver-timeout-assertion 0
     function check_mulWadUpDoesntRevert() public pure {
         uint256 x = svm.createUint256("x");
-	uint256 y = svm.createUint256("y");
-	require(x == 0 || y == 0 || y <= type(uint256).max / x);
+        uint256 y = svm.createUint256("y");
+        require(x == 0 || y == 0 || y <= type(uint256).max / x);
         uint256 result = MathMasters.mulWadUp(x, y);
         uint256 expected = x * y == 0 ? 0 : (x * y - 1) / 1e18 + 1;
         assert(result == expected);
         
     }
-    // function check_mulWadUpDoesntRevert(uint256 x, uint256 y) public pure {
-        // require(x == 0 || y == 0 || y <= type(uint256).max / x);
-        //     uint256 result = MathMasters.mulWadUp(x, y);
-        //     uint256 expected = x * y == 0 ? 0 :` (x * y - 1) / 1e18 + 1;
-        //     assert(result == expected);
-    // }
+
+    function check_sqrtIsCorrect() public {}
+    //function check_mulWadUpDoesntRevert(uint256 x, uint256 y) public pure {
+        //require(x == 0 || y == 0 || y <= type(uint256).max / x);
+        //uint256 result = MathMasters.mulWadUp(x, y);
+        //uint256 expected = x * y == 0 ? 0 : (x * y - 1) / 1e18 + 1;
+        //assert(result == expected);
+    //}
 }
